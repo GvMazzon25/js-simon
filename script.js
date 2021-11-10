e6661be1589cafbe4d2e8f3ee89a5212ccee0406
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-10 16:38:08 +0100

    generation-number

--- script.js
+++ script.js
@@ -6,36 +6,60 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 const numbers = [
     {
-        number1: ''  
+        number1: genRandomNumber()  
     },
     {
-        number2:``
+        number2:  genRandomNumber()
     },
     {
-        number2:``
+        number3:  genRandomNumber()
     },
     {
-        number2:``
+        number4:  genRandomNumber()
     },
     {
-        number3:``
+        number5:  genRandomNumber()
     },
 ]
 
 console.log(numbers);
 
-const max = 100;
-const min = 1;
+const numbersContainer = document.querySelector('.container');
 
+function genPost(numbers, numbersContainer){
+    numbersContainer.innerHTML = '';
+
+    for(let i = 0; i <= numbers.length; i++){
+        const numbersItem = numbers[i];
+        console.log(numbersItem);
+    
+        numbersContainer.innerHTML += `
+        <div class="number">
+        <h1>${numbersItem.number1}</h1>
+        </div>
+        <div class="number">
+            <h1>${numbersItem.number2}</h1>
+        </div>
+        <div class="number">
+            <h1>${numbersItem.number3}</h1>
+        </div>
+        <div class="number">
+            <h1>${numbersItem.number4}</h1>
+        </div>
+        <div class="number">
+            <h1>${numbersItem.number5}</h1>
+        </div>`
+    }
+}
+
+genPost(numbers, numbersContainer);
 
 
 //Function
 
 
-let userNumber = '';
-
-function genRandomNumber(max, min){
-    userNumber = Math.floor(Math.random()*max)-min;
+function genRandomNumber(){
+    let userNumber = Math.floor(Math.random()*100);
     return userNumber
 }
 
